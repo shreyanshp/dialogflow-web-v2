5d1b9b865c67a592e3c0029540e16783feccf712
mishushakov <mishushakov@users.noreply.github.com>
2019-11-13 14:40:05 +0000

    Deploy

---
+++ precache-manifest.1e9711974ba34eb10b93b5cfb3666b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72bd01dc1fd49705b82e",
+    "url": "css/app.727e4ce5.css"
+  },
+  {
+    "revision": "72bd01dc1fd49705b82e",
+    "url": "js/app.9afd7841.js"
+  },
+  {
+    "revision": "c00aa80d34b334474459",
+    "url": "js/chunk-vendors.2d1eee83.js"
+  },
+  {
+    "revision": "4e24baaebc8040f9f8f813a585ca334d",
+    "url": "index.html"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  }
+];
